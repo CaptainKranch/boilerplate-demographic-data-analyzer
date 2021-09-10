669a52052031ee8e366886d8600c8a31a42e5094
Daniel Garcia M <daniel.garcia9@udea.edu.co>
2021-09-10 11:00:09 -0500

    :100:

--- testing.py
+++ testing.py
@@ -1,11 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('adult.data.csv')
-#
-# race_count = df['race'].value_counts()
-#
-# average_age_men = df[df['sex'] == 'Male'].mean()
 
-percentage_bachelors = df[df['education'] == 'Bachelors'].value_counts()
-percentage_bachelors = ((percentage_bachelors/len(df)) * 100)
-print(percentage_bachelors)
+average_age_men = df[df['sex'] == 'Male']['age'].mean()
+print(average_age_men)
